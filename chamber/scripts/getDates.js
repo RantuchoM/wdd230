df71cf260c9cbc23ede158ef893c8ec797fb030e
RantuchoM <martin.rantucho@gmail.com>
2024-02-05 17:57:06 -0300

    W04 - Join

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,3 +1,5 @@
 document.getElementById("currentYear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+document.getElementById("timestamp").value = new Date().toISOString();
