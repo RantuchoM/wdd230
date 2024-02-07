67d26c3c702e4d4e812de1ddeea6b99a3237e2f8
RantuchoM <martin.rantucho@gmail.com>
2024-02-07 17:31:46 -0300

    W05

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,46 @@
-const baseURL = "https://rantuchom.github.io/wdd230/";
-const linksUrl = `${baseURL}/data/links.json`
+document.addEventListener("DOMContentLoaded", function () {
+  const baseURL = "https://rantuchom.github.io/wdd230";
+  const linksUrl = `${baseURL}/data/links.json`
+  const section = document.getElementById('weekMenu');
 
-async function getLinks() {
+  async function getLinks() {
     const response = await fetch(linksUrl);
     const data = await response.json();
     console.log(data);
+    displayLinks(data.weeks);
   }
-  
+
+  function displayLinks(weeks) {
+    weeks.forEach(week => {
+      let li = document.createElement('li');
+      let aList = [];
+
+      // Create the week text
+      let weekText = document.createTextNode(`${week.week}: `);
+      li.appendChild(weekText);
+
+      // Create <a> elements for each link
+      week.links.forEach(link => {
+        let a = document.createElement('a');
+        a.href = link.url;
+        a.textContent = link.title;
+        aList.push(a);
+      });
+
+      // Append <a> elements to the <li> joined by " | "
+      aList.forEach((a, index) => {
+        li.appendChild(a);
+        if (index < aList.length - 1) {
+          let separator = document.createTextNode(" | ");
+          li.appendChild(separator);
+        }
+      });
+
+      // Append the <li> to the section
+      section.appendChild(li);
+    });
+  }
+
+
   getLinks();
+});
