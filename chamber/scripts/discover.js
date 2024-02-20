f1eff43e992ec19d3ea79b8c71c4d69879a2f581
RantuchoM <martin.rantucho@gmail.com>
2024-02-20 02:12:19 -0300

    JSON

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,41 +1,41 @@
 document.addEventListener("DOMContentLoaded",function (){
 
 
-    const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+    const url = 'https://rantuchom.github.io/wdd230/chamber/data/members.json';
     const cards = document.querySelector('#cards');
 
 
 
-    async function getProphetData() {
+    async function getCompanyData() {
         const results = await fetch(url);
         const data = await results.json();
-        console.table(data.prophets);
-        displayProphets(data.prophets);
+        console.table(data.companies);
+        displayCompanies(data.companies);
     }
-    const displayProphets = (prophets) => {
-        prophets.forEach((prophet) => {
+    const displayCompanies = (companies) => {
+        companies.forEach((company) => {
             let card = document.createElement('section');
-            let fullName = document.createElement('h2'); 
+            let name = document.createElement('h2'); 
             let img = document.createElement('img');
-            let birthDate = document.createElement('p');
-            let birthPlace = document.createElement('p');
+            let address = document.createElement('p');
+            let phone = document.createElement('p');
 
-            // Build the h2 content out to show the prophet's full name
-            fullName.textContent = `${prophet.name} ${prophet.lastname}`; 
+            // Build the h2 content out to show the company's full name
+            name.textContent = company.name; 
             // Build the image portrait by setting all the relevant attributes
-            img.setAttribute('src', prophet.imageurl);
-            img.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+            img.setAttribute('src', company.image);
+            img.setAttribute('alt', `Logo of ${company.name}`);
             img.setAttribute('loading', 'lazy');
             img.setAttribute('width', '340');
             img.setAttribute('height', '440');
 
-            birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
-            birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+            address.textContent = `Address: ${company.address}`;
+            phone.textContent = `Phone: ${company.birthplace}`;
 
             // Append the section(card) with the created elements
-            card.appendChild(fullName); 
-            card.appendChild(birthDate);
-            card.appendChild(birthPlace);
+            card.appendChild(name); 
+            card.appendChild(address);
+            card.appendChild(phone);
             card.appendChild(img);
 
             cards.appendChild(card);
@@ -43,5 +43,5 @@ document.addEventListener("DOMContentLoaded",function (){
         })
     }
 
-    getProphetData();
+    getCompanyData();
 });
